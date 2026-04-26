cbdf8db27b07c5d48a377bc0dab25b49efb35c6e
iamaffanyousuf <iamaffanyousuf01@gmail.com>
2026-04-27 00:00:45 +0530

    setup project structure

--- src/test_loader.py
+++ src/test_loader.py
@@ -1,15 +1,21 @@
 from utils import load_config
 from dataset import get_dataloaders
 
-config = load_config("configs/config.yaml")
 
-train_loader, val_loader, test_loader = get_dataloaders(config)
+def main():
+    config = load_config("configs/config.yaml")
 
-print("Train batches:", len(train_loader))
-print("Val batches:", len(val_loader))
-print("Test batches:", len(test_loader))
+    train_loader, val_loader, test_loader = get_dataloaders(config)
 
-# Check one batch
-images, labels = next(iter(train_loader))
-print("Image shape:", images.shape)
-print("Labels:", labels[:10])
+    print("Train batches:", len(train_loader))
+    print("Val batches:", len(val_loader))
+    print("Test batches:", len(test_loader))
+
+    # Check one batch
+    images, labels = next(iter(train_loader))
+    print("Image shape:", images.shape)
+    print("Labels:", labels[:10])
+
+
+if __name__ == "__main__":
+    main()
